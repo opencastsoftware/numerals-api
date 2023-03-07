38dbe98796f29a7a1b95331d0206f37d7c14bdd9
Pete Smith <peter.smith@opencastsoftware.com>
2023-03-07 10:23:39 +0000

    save

--- src/main/java/com/opencastsoftware/numeralsapi/service/ConverterService.java
+++ src/main/java/com/opencastsoftware/numeralsapi/service/ConverterService.java
@@ -2,10 +2,38 @@ package com.opencastsoftware.numeralsapi.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.TreeMap;
+
 @Service
 public class ConverterService {
 
     public String convertNumberToNumberal(int numberToConvert) {
-        return "L";
+        TreeMap<Integer, String> numeralMap = new TreeMap<>();
+        numeralMap.put(1, "I");
+        numeralMap.put(5, "V");
+        numeralMap.put(10, "X");
+        numeralMap.put(50, "L");
+        numeralMap.put(100, "C");
+        numeralMap.put(500, "D");
+        numeralMap.put(1000, "M");
+
+        numeralMap.put(4, "IV");
+        numeralMap.put(9, "IX");
+        numeralMap.put(40, "XL");
+        numeralMap.put(90, "XC");
+        numeralMap.put(400, "CD");
+        numeralMap.put(900, "CM");
+
+        int flooredKey = numeralMap.floorKey(numberToConvert);
+        if (flooredKey == numberToConvert){
+            return numeralMap.get(numberToConvert);
+        } else {
+            
+        }
+
+
+
     }
+
 }
